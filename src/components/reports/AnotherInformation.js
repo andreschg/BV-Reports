551835f3e8869a343e616187570c33d3b6753742
Andres Chaves <andres1009_93@hotmail.com>
2018-06-15 16:47:33 -0600

    Added education information to report.

--- src/components/reports/AnotherInformation.js
+++ src/components/reports/AnotherInformation.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
+import EducationReport from './EducationReport';
 
 const AnotherInformation = (props) => (
   <Tabs id="another-info-tabs">
     <Tab title="Education">
-      
+      <EducationReport education={props.data.educations} />
     </Tab>
     <Tab title="Jobs">
 
---
+++ src/components/reports/SingleEducationReport.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Panel } from 'react-bootstrap';
+import moment from 'moment';
+
+const SingleEducationReport = ({ school, degree, attended }) => {
+  const startDate = moment(attended.start);
+  const endDate = moment(attended.end);
+  const format = 'MMMM YYYY';
+  let messaage = 'No data';
+  if (startDate.isValid() && endDate.isValid())
+    messaage = `From ${startDate.format(format)} to ${endDate.format(format)}`;
+  else if (startDate.isValid() && !endDate.isValid())
+    messaage = `Since ${startDate.format(format)}`;
+
+  return (
+    <Panel>
+      <Panel.Heading>
+        {messaage}
+      </Panel.Heading>
+      <Panel.Body> 
+        <p><label>School: </label> {school}</p>
+        <p><label>Degree: </label> {degree}</p>
+      </Panel.Body>
+    </Panel>
+  );
+}
+
+export default SingleEducationReport;
