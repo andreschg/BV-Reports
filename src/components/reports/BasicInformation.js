563bba1702fd9711aa54afee226abd4da4316361
Andres Chaves <andres1009_93@hotmail.com>
2018-06-18 17:06:34 -0600

    Added styles for the report basic information component.

--- src/components/reports/BasicInformation.js
+++ src/components/reports/BasicInformation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Image, Col, Carousel, Button } from 'react-bootstrap';
+import { Panel, Row, Image, Col, Carousel, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import dataService from '../../services/dataService';
@@ -13,52 +13,50 @@ class BasicInformation extends React.Component{
 
   render() {
     return (
-      <div>
-        <Carousel controls={false}>
-          {this.props.data.images.map((element, index) => (
-          <Carousel.Item key={`pic-${index}`} className="profilePic-container">
-            <Image src={element.url} rounded responsive />
-          </Carousel.Item>
-          ))}
-        </Carousel>
-        <h2>Basic Information</h2>
-        <Row>
-          <Col md={3}><label>Names:</label></Col>
-          <Col md={9}>
-            <ul>
-              { this.props.data.names.map((element, index) => (
-                <li key={`name-${index}`}>{element.full}</li>
-              ))} 
-            </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={3}><label>Email addresses:</label></Col>
-          <Col md={9}>
-            <ul>
-              { this.props.data.emails.map((element, index) => (
-                <li key={`email-${index}`}>{element.email_address}</li>
-              ))} 
-            </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={3}><label>Phones:</label></Col>
-          <Col md={9}>
-            <ul>{ this.props.data.phones.map((element, index) => (
-              <li key={`phone-${index}`}>{element.number}</li>
+      <Panel className="report_basic-information">
+        <Panel.Heading className="green-underline">
+          <h3>Basic Information</h3>
+        </Panel.Heading>
+        <Panel.Body>
+          <Carousel className="report-pics" controls={false}>
+            {this.props.data.images.map((element, index) => (
+            <Carousel.Item key={`pic-${index}`} className="profilePic-container">
+              <Image src={element.url} responsive />
+            </Carousel.Item>
             ))}
-            </ul>
-          </Col>
-        </Row>
-        { this.props.isNew && (
-          <Row>
-            <Col md={12}>
-              <Button bsStyle="success" onClick={this.saveReport} className="btn-full" >Save Report</Button>
+          </Carousel>
+            <Col md={4}><label>Names:</label></Col>
+            <Col md={8}>
+              <ul className="ul-list">
+                { this.props.data.names.map((element, index) => (
+                  <li key={`name-${index}`}>{element.full}</li>
+                ))} 
+              </ul>
             </Col>
-          </Row>
-        )}
-      </div>
+            <Col md={4}><label>Email addresses:</label></Col>
+            <Col md={8}>
+              <ul className="ul-list">
+                { this.props.data.emails.map((element, index) => (
+                  <li key={`email-${index}`}>{element.email_address}</li>
+                ))} 
+              </ul>
+            </Col>
+            <Col md={4}><label>Phones:</label></Col>
+            <Col md={8}>
+              <ul className="ul-list">{ this.props.data.phones.map((element, index) => (
+                <li key={`phone-${index}`}>{element.number}</li>
+              ))}
+              </ul>
+            </Col>
+          { this.props.isNew && (
+            <Row>
+              <Col md={12}>
+                <Button bsStyle="success" onClick={this.saveReport} className="btn-full" >Save Report</Button>
+              </Col>
+            </Row>
+          )}
+        </Panel.Body>
+      </Panel>
     );
   }
 }
